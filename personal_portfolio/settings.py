3848b8a144ae2501c8891f1888dc108fd4c2c854
Volodymyr Popov <volodymyr.popov@megogo.net>
2022-09-16 21:47:46 +0200

    append to allowed hosts new host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-be+tsnrkub@jwi-)3px*dd(f8*wx8fas-d+kzch4g$6sw291)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personalportfolio.pythonanywhere.com/']
 
 
 # Application definition
